232174a44fdb9bb968bab17e30bf9888c48f6d78
Quan Wang <quanw@google.com>
2019-01-27 10:53:01 -0500

    Add setup.py and publish.sh

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""Setup script for the package."""
+
+import setuptools
+
+VERSION = "0.0.1"
+
+with open("README.md", "r") as file_object:
+    LONG_DESCRIPTION = file_object.read()
+
+SHORT_DESCRIPTION = """
+A lightweight library to compute Diarization Error Rate (DER).
+""".strip()
+
+setuptools.setup(
+    name="simpleder",
+    version=VERSION,
+    author="Quan Wang",
+    author_email="quanw@google.com",
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wq2012/SimpleDER",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
