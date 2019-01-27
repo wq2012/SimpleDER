cf86ce0d663b985a09a7aba8ee0387e33032ae64
Quan Wang <quanw@google.com>
2019-01-27 09:44:28 -0500

    Complete TestComputeIntersectionLength

--- tests/det_test.py
+++ tests/det_test.py
@@ -12,9 +12,19 @@ class TestComputeIntersectionLength(unittest.TestCase):
     """Tests for the compute_intersection_length function."""
 
     def test_include(self):
-        A = [1.0, 5.0]
-        B = [2.0, 3.0]
+        A = ["A", 1.0, 5.0]
+        B = ["B", 2.0, 3.0]
         self.assertEqual(1.0, der.compute_intersection_length(A, B))
+
+    def test_separate(self):
+        A = ["A", 1.0, 3.0]
+        B = ["B", 5.0, 7.0]
+        self.assertEqual(0.0, der.compute_intersection_length(A, B))
+
+    def test_overlap(self):
+        A = ["A", 1.0, 5.0]
+        B = ["B", 2.0, 9.0]
+        self.assertEqual(3.0, der.compute_intersection_length(A, B))
 
 
 if __name__ == "__main__":
